bb5dc629433b234bc5805ec1699aa3626f9e1cd9
developer@yanceyleo.com <developer@yanceyleo.com>
2018-08-25 13:54:56 +0800

    🐯add 「shuffle」and 「deduplicateArray」 functions

--- tools.js
+++ tools.js
@@ -3,7 +3,7 @@
  *  return array
  * */
 export const getType = (data) => {
-  return Object.prototype.toString.call(data).slice(8, -1).toLowerCase();
+    return Object.prototype.toString.call(data).slice(8, -1).toLowerCase();
 };
 
 /**
@@ -11,8 +11,27 @@ export const getType = (data) => {
  *  return 2018-08-10 11:09:14
  * */
 export const formatJSONDate = (jsonDate) => {
-  return new Date(+new Date(new Date(jsonDate).toJSON()) + 8 * 3600 * 1000).toISOString()
-    .replace(/T/g, ' ').replace(/\.[\d]{3}Z/, '');
+    return new Date(+new Date(new Date(jsonDate).toJSON()) + 8 * 3600 * 1000).toISOString()
+        .replace(/T/g, ' ').replace(/\.[\d]{3}Z/, '');
 };
 
+/**
+ *  deduplicateArray([4, 2, 2, 4, 1])
+ *  return [ 4, 2, 1 ]
+ * */
+export const deduplicateArray = (arr) => {
+    return [...new Set(arr)];
+}
 
+/**
+ *  shuffle([1, 2, 3, 4, 5])
+ *  return [ 4, 2, 1, 5, 3 ]
+ * */
+export const shuffle = function (arr) {
+    var i = arr.length, j;
+    while (i) {
+        j = Math.floor(Math.random() * i--);
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+}
