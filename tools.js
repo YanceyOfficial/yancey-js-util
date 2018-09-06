0fedca2d6907399eeb95537765e33d98b7fe29a1
developer@yanceyleo.com <developer@yanceyleo.com>
2018-09-06 18:48:41 +0800

    update

--- tools.js
+++ tools.js
@@ -1,108 +1,92 @@
 /**
- *  @param {Any} param
- *  @example getType([1, 2, 3])
- *  @return array
+ *  @param {Object} param
+ *  @description To get the type of param.
  * */
 export const getType = (param) => {
-    return Object.prototype.toString.call(param).slice(8, -1).toLowerCase();
+  return Object.prototype.toString.call(param).slice(8, -1).toLowerCase();
 };
 
 /**
  *  @param {String} jsonDate
- *  @example formatJSONDate('2018-08-10T03:09:14.127086Z')
- *  @return 2018-08-10 11:09:14
+ *  @description Convert JSONDate to common.
  * */
 export const formatJSONDate = (jsonDate) => {
-    return new Date(+new Date(new Date(jsonDate).toJSON()) + 8 * 3600 * 1000).toISOString()
-        .replace(/T/g, ' ').replace(/\.[\d]{3}Z/, '');
+  return new Date(+new Date(new Date(jsonDate).toJSON()) + 8 * 3600 * 1000).toISOString()
+    .replace(/T/g, ' ').replace(/\.[\d]{3}Z/, '');
 };
 
 /**
- *  @param {String} arr
- *  @example deduplicateArray([4, 2, 2, 4, 1])
- *  @return [ 4, 2, 1 ]
+ *  @param {Array} arr
+ *  @description Remove deduplicate elements at an array.
  * */
 export const deduplicateArray = (arr) => {
-    return [...new Set(arr)];
+  return [...new Set(arr)];
 };
 
 /**
- *  shuffle([1, 2, 3, 4, 5])
- *  return [ 4, 2, 1, 5, 3 ]
+ *  @param {Array} arr
+ *  @description Shuffle an array.
  * */
 export const shuffle = function (arr) {
-    let i = arr.length, j;
-    while (i) {
-        j = Math.floor(Math.random() * i--);
-        [arr[i], arr[j]] = [arr[j], arr[i]];
-    }
-    return arr;
+  let i = arr.length, j;
+  while (i) {
+    j = Math.floor(Math.random() * i--);
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
 };
 
 /**
  *  @param {String} str
  *  @param {String} currencies
- *  @example toThousands('1234567.85', '¥')
- *  @return ¥1,234,567.85
+ *  @description Make amount to thousands.
  * */
 export const toThousands = (str, currencies = '$') => {
-    if (!parseFloat(str)) {
-        return 'Please input numeric string';
-    } else {
-        return currencies + str.replace(/(\d{1,3})(?=(?:\d{3})+(?!\d))/g, '$1,');
-    }
+  return currencies + str.replace(/(\d{1,3})(?=(?:\d{3})+(?!\d))/g, '$1,');
 };
 
 /**
  *  @param {Number} num
- *  @example simpleToThousands(1234567.85, '¥')
- *  @return ¥1,234,567.85
+ *  @param {String} currencies
+ *  @description The other method to make amount to thousands.
  * */
 export const simpleToThousands = (num, currencies = '$') => {
-  if (!parseFloat(num)) {
-    return 'Please input number';
-  } else {
-    return currencies + num.toLocaleString('en-US');
-  }
+  return currencies + num.toLocaleString('en-US');
 };
 
 /**
  *  @param {String} str
- *  @example capitalized('hello, world!')
- *  @return Hello, World!
+ *  @description Capitalize the first letter of each word.
  * */
 export const capitalized = (str) => {
-    return str.toLowerCase().replace(/( |^)[a-z]/g, (L) => L.toUpperCase());
+  return str.toLowerCase().replace(/( |^)[a-z]/g, (L) => L.toUpperCase());
 };
 
 /**
  *  @param {Number} min
  *  @param {Number} max
- *  @example getRandomInt(1, 10)
- *  @return 6
+ *  @description  The maximum is exclusive and the minimum is inclusive.
  * */
 export const getRandomInt = (min, max) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; // The maximum is exclusive and the minimum is inclusive
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
 };
 
 /**
  *  @param {Number} min
  *  @param {Number} max
- *  @example getRandomIntInclusive(1, 10)
- *  @return 10
+ *  @description Both of The maximum and minimum are inclusive.
  * */
 export const getRandomIntInclusive = (min, max) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; // Both of The maximum and minimum are inclusive
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 /**
  *  @param {Array} arr
- *  @example deepFlatten(['a', ['b', 'c'], [['d', 'e']], ['f']])
- *  @return [ 'a', 'b', 'c', 'd', 'e', 'f' ]
+ *  @description Deep flatten an array.
  * */
 export const deepFlatten = (arr) => {
   const flatten = (arr) => [].concat(...arr);
@@ -110,8 +94,7 @@ export const deepFlatten = (arr) => {
 };
 
 /**
- *  @example checkWebp()
- *  @return true
+ *  @description Detect whether the browser supports webp.
  * */
 export const checkWebp = () => {
   return (document.createElement('canvas')
