a65a8530412e6cb319d05e807d69fc0525f0e995
YanceyOfficial <yanceyofficial@gmail.com>
2019-11-05 15:05:47 +0800

    feat: update version

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,14 @@
 import typescript from 'rollup-plugin-typescript'
 import { uglify } from 'rollup-plugin-uglify'
+import filesize from 'rollup-plugin-filesize'
 
 export default {
   input: 'src/index.ts',
-  plugins: [typescript({ lib: ['esnext'], target: 'es5' }), uglify()],
+  plugins: [
+    typescript({ lib: ['esnext'], target: 'es5' }),
+    uglify(),
+    filesize(),
+  ],
   output: {
     file: 'lib/index.js',
     format: 'cjs',
